880c95200f70aa086a9ba75772dd685e3f456681
Mike Zhang <mzhang6@gmail.com>
2013-08-15 23:33:56 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+StackOfPigs::Application.config.secret_key_base = '35e928b2ed2bd6489709d5f77eebab1d47b5f481e458bdf59747c3b439a8bfc202b9027c577c8e862cf04e66bc863b864ef579e7e2f88db16e29dc183ce45dc5'
