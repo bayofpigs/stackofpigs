e035d80a300d9e80add760f3be28a24a0f6e1658
Mike Zhang <mzhang6@gmail.com>
2013-08-23 11:55:16 -0400

    Added a side panel

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,4 +9,11 @@ module ApplicationHelper
       "#{base_title} | #{page_title}"
     end
   end
+
+  # Fetches the Gravatar for the given email
+  def gravatar_for(email)
+    gravatar_id = Digest::MD5::hexdigest(email.downcase)
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+    image_tag(gravatar_url, alt: "Gravatar", class: "gravatar")
+  end
 end
